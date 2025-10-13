f7ec28ca11b34d20a9a691bb680d3f3e2ee5391b
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2025-10-13 08:42:59 +0200

    Created an outline for quiz elements

--- Quiz.jsx
+++ Quiz.jsx
@@ -1,17 +1,30 @@
+import {decode} from 'html-entities';
+
 export default function Quiz(props){
     console.log(props.data)
-    return (
-        <div className="quiz-section">
-        <section className="quiz-box">
-        <h2></h2>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+    
+    const quizEl = props.data.map(function(quiz){
+        return <section className="quiz-box">
+        <h2>{decode(quiz.question)}</h2>
+        <button>{decode(quiz.correct_answer)}</button>
+        <button>{decode(quiz.incorrect_answers[0])}</button>
+        <button>{decode(quiz.incorrect_answers[1])}</button>
+        <button>{decode(quiz.incorrect_answers[2])}</button>
         <div className="grey-line"></div>
         </section>
+    })
+    
+    
+    return (
+        <div className="quiz-section">
+        
+        {quizEl}
         
         <button className="check-answers">Check answers</button>
         </div>
     )
 }
+
+
+//  put the answers all into a randomised array
+//  track correct answer and turn it green 
