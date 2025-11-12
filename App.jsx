0e3a5a803ca47eec8b85dc808f70725d25c5bb28
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2025-11-12 12:12:12 +0100

    Commit new files of finished Quizzical

--- App.jsx
+++ App.jsx
@@ -3,23 +3,23 @@ import Intro from "./Intro.jsx";
 import Quiz from "./Quiz.jsx";
 
 export default function App() {
-  const [gameStart, setGameStart] = React.useState(false);
-  const [quiz, setQuiz] = React.useState();
+const [gameStart, setGameStart] = React.useState(false);
+const [quiz, setQuiz] = React.useState([]);
 
-  async function renderGame() {
+async function renderGame() {
     const res = await fetch(
-      "https://opentdb.com/api.php?amount=5&type=multiple"
+    "https://opentdb.com/api.php?amount=5&type=multiple"
     );
     const data = await res.json();
     setQuiz(data.results);
     setGameStart(true);
-  }
+}
 
-  return (
+return (
     <main>
-      {!gameStart && <Intro onClick={renderGame} />}
+    {!gameStart && <Intro onClick={renderGame} />}
 
-      {gameStart && <Quiz data={quiz} />}
+    {gameStart && <Quiz data={quiz} reload={renderGame} />}
     </main>
-  );
+);
 }
--- Quiz.jsx
+++ Quiz.jsx
@@ -1,30 +1,135 @@
-import {decode} from 'html-entities';
+import { decode } from "html-entities";
+import { clsx } from "clsx";
+import { nanoid } from "nanoid";
+import React from "react";
 
-export default function Quiz(props){
-    console.log(props.data)
-    
-    const quizEl = props.data.map(function(quiz){
-        return <section className="quiz-box">
-        <h2>{decode(quiz.question)}</h2>
-        <button>{decode(quiz.correct_answer)}</button>
-        <button>{decode(quiz.incorrect_answers[0])}</button>
-        <button>{decode(quiz.incorrect_answers[1])}</button>
-        <button>{decode(quiz.incorrect_answers[2])}</button>
-        <div className="grey-line"></div>
-        </section>
-    })
-    
-    
+export default function Quiz(props) {
+  const [questionsArray, setQuestionsArray] = React.useState([]);
+  const [showResults, setShowResults] = React.useState(false);
+  let correctScore = questionsArray.filter((question) => {
     return (
-        <div className="quiz-section">
-        
-        {quizEl}
-        
-        <button className="check-answers">Check answers</button>
-        </div>
-    )
+      question.selectedIndex !== null &&
+      question.answers[question.selectedIndex] === question.correctAnswer
+    );
+  }).length;
+
+  function setObjects() {
+    if (!Array.isArray(props.data) || props.data.length === 0) return [];
+
+    let newarray = [];
+    props.data.forEach(function (quiz) {
+      newarray.push({
+        question: quiz.question,
+        answers: [quiz.correct_answer, ...quiz.incorrect_answers],
+        correctAnswer: quiz.correct_answer,
+        selectedIndex: null,
+        id: nanoid(),
+      });
+    });
+
+    newarray.forEach(function (question) {
+      shuffleAnswer(question.answers);
+    });
+
+    function shuffleAnswer(combined) {
+      for (let i = combined.length - 1; i >= 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = combined[i];
+        combined[i] = combined[j];
+        combined[j] = temp;
+      }
+      return combined;
+    }
+    return newarray;
+  }
+
+  function holdAnswer(questionIndex, answerIndex) {
+    setQuestionsArray(function (prevValue) {
+      return prevValue.map(function (question, index) {
+        if (index === questionIndex) {
+          return {
+            ...question,
+            selectedIndex: answerIndex,
+          };
+        } else {
+          return { ...question };
+        }
+      });
+    });
+  }
+
+  function checkAnswer() {
+    if (!showResults) {
+      setShowResults(true);
+    } else {
+      props.reload();
+    }
+  }
+
+  const quizEl = questionsArray.map(function (question, questionIndex) {
+    return (
+      <section key={question.id} className="quiz-box">
+        <h2>{decode(question.question)}</h2>
+        {question.answers.map(function (answer, answerIndex) {
+          const isCorrect = showResults && answer === question.correctAnswer;
+          const isWrong = showResults && !isCorrect;
+          const isSelected = answerIndex === question.selectedIndex;
+          const isDisabled = showResults;
+
+          return (
+            <button
+              key={question.id + answer}
+              disabled={showResults}
+              className={clsx(
+                "answer-btn",
+                isSelected && "selected",
+                isCorrect && "correct",
+                isWrong && isSelected && "wrong",
+                isDisabled && isWrong && !isSelected && "disabled"
+              )}
+              onClick={() => {
+                holdAnswer(questionIndex, answerIndex);
+              }}
+            >
+              {decode(answer)}
+            </button>
+          );
+        })}
+        <div className="grey-line"></div>
+      </section>
+    );
+  });
+
+  React.useEffect(() => {
+    if (!Array.isArray(props.data) || props.data.length === 0) return;
+
+    const newArr = setObjects();
+    setQuestionsArray(newArr);
+    setShowResults(false);
+  }, [props.data]);
+
+  if (questionsArray.length === 0) {
+    return <p>loading quiz...</p>;
+  }
+
+  return (
+    <div className="quiz-section">
+      {quizEl}
+      <div className="results-box">
+        {showResults && (
+          <p className="score">
+            You scored {correctScore} / {questionsArray.length} correct answers
+          </p>
+        )}
+        <button
+          className="check-answers"
+          onClick={() => {
+            checkAnswer();
+          }}
+        >
+          {showResults ? "Play Again" : "Check answers"}
+        </button>
+      </div>
+    </div>
+  );
 }
-
-
-//  put the answers all into a randomised array
-//  track correct answer and turn it green 
